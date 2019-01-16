0d589bb6ccec70a0ffe0a5876e0d22e0104e90f9
justin4480 <justin4480@gmail.com>
2019-01-16 00:42:34 +0000

    Update Character class

--- fiesta_fight.py
+++ fiesta_fight.py
@@ -1,12 +1,25 @@
 
 class Character:
 
-    def __init__(self, name):
+    """
+    Base Character Object
+    """
+
+    MAX_LEVEL = 10
+
+    def __init__(self, name, health = 100, level = 1):
         self.name = name
-        self.health = 1.0
+        self.type = type
+        self.stats = {'attack':}
+        self.skill = {}
+        self.health = health
+        self.level = level
 
     def speak(self):
         return str("hello, I'm " + self.name)
+
+    def level_up(self, max_level):
+        if self.level < max_level: self.level =+ 1
 
 
 def main():
