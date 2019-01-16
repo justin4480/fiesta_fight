f2db6baab4b871b99e7d780d176f6a07b462af29
justin4480 <justin4480@gmail.com>
2019-01-16 23:54:38 +0000

    character generation

--- fiesta_fight.py
+++ fiesta_fight.py
@@ -1,3 +1,6 @@
+import random
+import pandas as pd
+
 
 class Character:
 
@@ -7,13 +10,11 @@ class Character:
 
     MAX_LEVEL = 10
 
-    def __init__(self, name, health = 100, level = 1):
+    def __init__(self, name, stats):
         self.name = name
-        self.type = type
-        self.stats = {'attack':}
-        self.skill = {}
-        self.health = health
-        self.level = level
+        self.stats = stats
+        self.health = 100
+        self.level = 1
 
     def speak(self):
         return str("hello, I'm " + self.name)
@@ -23,8 +24,36 @@ class Character:
 
 
 def main():
-    justin = Character("Justin")
-    print(justin.speak())
+    tom = Character("tom", {'attack': 5, 'defense': 5, 'stamina': 5})
+    print(tom.speak())
+
+    jerry = Character("jerry", {'attack': 5, 'defense': 5, 'stamina': 5})
+    print(jerry.speak())
+
+    itchy = Character("itchy", {'attack': 5, 'defense': 5, 'stamina': 5})
+    print(itchy.speak())
+
+    scratchy = Character("scratchy", {'attack': 5, 'defense': 5, 'stamina': 5})
+    print(scratchy.speak())
+
+    def generate_random_stats():
+        rand_numbers = []
+        for _ in range(3):
+            rand_numbers.append(random.randint(0, 15-sum(rand_numbers)))
+
+        random.shuffle(rand_numbers)
+        return {
+            'attack': rand_numbers[0],
+            'defense': rand_numbers[1],
+            'stamina': rand_numbers[2]
+        }
+
+    stat_list = []
+    for i in range(10):
+        stat_list.append(generate_random_stats())
+
+    df = pd.DataFrame(stat_list)
+    df.total = df.apply(lambda x: x.attack + x.defense + x.stamina)
 
 
 if __name__ == "__main__":
---
+++ testing.py
@@ -0,0 +1,3 @@
+d = {'attack': 5, 'defense': 5, 'stamina': 5}
+
+print(d.get('attack'))
